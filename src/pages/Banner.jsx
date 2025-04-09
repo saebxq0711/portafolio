0625573bf95fb51c067c71618bd3919eca465011
Sebastian A <jsebaslozano2006@gmail.com>
2025-04-09 12:20:49 -0500

    portafolio

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,75 +1,69 @@
 import { Container, Row, Col } from "react-bootstrap";
 import header from '../assets/img/header-img.svg';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import TrackVisibility from "react-on-screen";
 import "animate.css";
 
 const Banner = () => {
-    const [loopNum, setLoopNum] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const toRotate = ["Desarrollador Web", "Desarrollador FullStack", "Desarrollador de Software"];
-    const [text, setText] = useState('');
-    const [delta, setDelta] = useState(300 - Math.random() * 100);
-    const period = 2000;
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRotate = ["Desarrollador Web", "Desarrollador FullStack", "Desarrollador de Software"];
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
 
-    useEffect(() => {
-        const tick = () => {
-            const i = loopNum % toRotate.length;
-            const fullText = toRotate[i];
-            const updatedText = isDeleting
-                ? fullText.substring(0, text.length - 1)
-                : fullText.substring(0, text.length + 1);
+  const tick = useCallback(() => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
-            setText(updatedText);
+    setText(updatedText);
 
-            if (isDeleting) {
-                setDelta((prevDelta) => prevDelta / 2);
-            }
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
 
-            if (!isDeleting && updatedText === fullText) {
-                setIsDeleting(true);
-                setDelta(period);
-            } else if (isDeleting && updatedText === "") {
-                setIsDeleting(false);
-                setLoopNum((prevLoopNum) => prevLoopNum + 1);
-                setDelta(500);
-            }
-        };
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(prevLoopNum => prevLoopNum + 1);
+      setDelta(500);
+    }
+  }, [text, isDeleting, loopNum, toRotate]);
 
-        const ticker = setInterval(() => {
-            tick();
-        }, delta);
+  useEffect(() => {
+    const ticker = setInterval(() => {
+      tick();
+    }, delta);
 
-        return () => clearInterval(ticker);
-    }, [text, delta, isDeleting, loopNum, toRotate]);
+    return () => clearInterval(ticker);
+  }, [tick, delta]);
 
-    return (
-        <section className="banner" id="home">
-            <Container>
-                <Row className="align-items-center">
-                    <Col xs={12} md={6} xl={7}>
-                        <TrackVisibility>
-                            {({ isVisible }) => (
-                                <div className={isVisible ? "animated_animated animate_fadeIn" : ""}>
-                                    <span className="tagline">Bienvenido a mi portafolio</span>
-                                    <h1>
-                                        {`Hola, soy Juan Aranda `}<span className="wrap">{text}</span>
-                                    </h1>
-                                    <p>
-                                        Soy un analista y desarrollador de software en etapa lectiva en el
-                                        Servicio Nacional de Aprendizaje (SENA)
-                                    </p>
-                                </div>
-                            )}
-                        </TrackVisibility>
-                    </Col>
-                    <Col xs={12} md={6} xl={5}>
-                        <img src={header} alt="Banner" />
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-    );
-};
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <span className="tagline">Bienvenido a mi portafolio</span>
+                  <h1>{`Hola, soy Juan Aranda `}<span className="wrap">{text}</span></h1>
+                  <p>Soy un analista y desarrollador de software en etapa lectiva en el Servicio Nacional de Aprendizaje (SENA)</p>
+                </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={header} alt="Banner" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
 
 export default Banner;
